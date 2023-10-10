274693d0d2354dc51117b2633b39cbd194eccc37
Rishabhjha9868 <147179883+Rishabhjha9868@users.noreply.github.com>
2023-10-10 11:05:51 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-const btnEl = document.getElementById("btn");
-const animeContainerEl = document.querySelector(".anime-container");
-const animeImgEl = document.querySelector(".anime-img");
-const amineNameEl = document.querySelector(".anime-name");
+const containerEl = document.querySelector(".container");
 
-btnEl.addEventListener("click", async function () {
-  try {
-    btnEl.disabled = true;
-    btnEl.innerText = "Loading...";
-    amineNameEl.innerText = "Updating...";
-    animeImgEl.src = "spinner.svg";
-    const response = await fetch("https://api.catboys.com/img");
-    const data = await response.json();
-    btnEl.disabled = false;
-    btnEl.innerText = "Get Anime";
-    animeContainerEl.style.display = "block";
-    animeImgEl.src = data.url;
-    amineNameEl.innerText = data.artist;
-  } catch (error) {
-    console.log(error);
-    btnEl.disabled = false;
-    btnEl.innerText = "Get Anime";
-    amineNameEl.innerText = "An error happened, please try again";
-  }
+const btnEl = document.querySelector(".btn");
+
+const popupContainerEl = document.querySelector(".popup-container");
+
+const closeIconEl = document.querySelector(".close-icon");
+
+btnEl.addEventListener("click", () => {
+  containerEl.classList.add("active");
+  popupContainerEl.classList.remove("active");
+});
+
+closeIconEl.addEventListener("click", () => {
+  containerEl.classList.remove("active");
+  popupContainerEl.classList.add("active");
 });
