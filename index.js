666a525e6eb72e94c3c9e8128ed155fd1ea7e1a1
Rishabhjha9868 <147179883+Rishabhjha9868@users.noreply.github.com>
2023-10-24 19:44:09 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-const hourEl = document.getElementById("hour");
-const minuteEl = document.getElementById("minutes");
-const secondEl = document.getElementById("seconds");
-const ampmEl = document.getElementById("ampm");
+const rating = document.querySelector(".emoji1");
+const ratingEls = document.querySelectorAll(".rating");
+const btnEl = document.getElementById("btn");
 
-function updateClock() {
-  let h = new Date().getHours();
-  let m = new Date().getMinutes();
-  let s = new Date().getSeconds();
-  let ampm = "AM";
+const containerEl = document.getElementById("container");
 
-  if (h > 12) {
-    h = h - 12;
-    ampm = "PM";
+let selectedRating = "";
+
+ratingEls.forEach((ratingEl) => {
+  ratingEl.addEventListener("click", (event) => {
+    removeActive();
+    selectedRating =
+      event.target.innerText || event.target.parentNode.innerText;
+    event.target.classList.add("active");
+    event.target.parentNode.classList.add("active");
+  });
+});
+
+btnEl.addEventListener("click", () => {
+  if (selectedRating !== "") {
+    containerEl.innerHTML = `
+        <strong>Thank you!</strong>
+        <br>
+        <br>
+        <strong>Feedback: ${selectedRating}</strong>
+        <p>We'll use your feedback to improve our customer support.</p>
+        `;
   }
+});
 
-  h = h < 10 ? "0" + h : h;
-  m = m < 10 ? "0" + m : m;
-  s = s < 10 ? "0" + s : s;
-
-  hourEl.innerText = h;
-  minuteEl.innerText = m;
-  secondEl.innerText = s;
-  ampmEl.innerText = ampm;
-  setTimeout(() => {
-    updateClock();
-  }, 1000);
+function removeActive() {
+  ratingEls.forEach((ratingEl) => {
+    ratingEl.classList.remove("active");
+  });
 }
-
-updateClock();
