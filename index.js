f83a1bbae21f8b153fda879d3256cd954c18c7fe
Rishabhjha9868 <147179883+Rishabhjha9868@users.noreply.github.com>
2023-10-31 21:04:46 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,64 +1,30 @@
-const btnEl = document.getElementById("btn");
-const appEl = document.getElementById("app");
+const containerEl = document.querySelector(".container");
 
-getNotes().forEach((note) => {
-  const noteEl = createNoteEl(note.id, note.content);
-  appEl.insertBefore(noteEl, btnEl);
-});
+for (let index = 0; index < 10; index++) {
+  const colorContainerEl = document.createElement("div");
+  colorContainerEl.classList.add("color-container");
+  containerEl.appendChild(colorContainerEl);
+}
 
-function createNoteEl(id, content) {
-  const element = document.createElement("textarea");
-  element.classList.add("note");
-  element.placeholder = "Empty Note";
-  element.value = content;
+const colorContainerEls = document.querySelectorAll(".color-container");
 
-  element.addEventListener("dblclick", () => {
-    const warning = confirm("Do you want to delete this note?");
-    if (warning) {
-      deleteNote(id, element);
-    }
+generateColors();
+
+function generateColors() {
+  colorContainerEls.forEach((colorContainerEl) => {
+    const newColorCode = randomColor();
+    colorContainerEl.style.backgroundColor = "#" + newColorCode;
+    colorContainerEl.innerText = "#" + newColorCode;
   });
-
-  element.addEventListener("input", () => {
-    updateNote(id, element.value);
-  });
-
-  return element;
 }
 
-function deleteNote(id, element) {
-  const notes = getNotes().filter((note) => note.id != id);
-  saveNote(notes);
-  appEl.removeChild(element);
+function randomColor() {
+  const chars = "0123456789abcdef";
+  const colorCodeLength = 6;
+  let colorCode = "";
+  for (let index = 0; index < colorCodeLength; index++) {
+    const randomNum = Math.floor(Math.random() * chars.length);
+    colorCode += chars.substring(randomNum, randomNum + 1);
+  }
+  return colorCode;
 }
-
-function updateNote(id, content) {
-  const notes = getNotes();
-  const target = notes.filter((note) => note.id == id)[0];
-  target.content = content;
-  saveNote(notes);
-}
-
-function addNote() {
-  const notes = getNotes();
-  const noteObj = {
-    id: Math.floor(Math.random() * 100000),
-    content: "",
-  };
-  const noteEl = createNoteEl(noteObj.id, noteObj.content);
-  appEl.insertBefore(noteEl, btnEl);
-
-  notes.push(noteObj);
-
-  saveNote(notes);
-}
-
-function saveNote(notes) {
-  localStorage.setItem("note-app", JSON.stringify(notes));
-}
-
-function getNotes() {
-  return JSON.parse(localStorage.getItem("note-app") || "[]");
-}
-
-btnEl.addEventListener("click", addNote);
