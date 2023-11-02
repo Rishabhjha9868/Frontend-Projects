587c5ea5c89b3cb467acfa1c68591b7d3e9b52e6
Rishabhjha9868 <147179883+Rishabhjha9868@users.noreply.github.com>
2023-11-02 19:39:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
-const btnEl = document.getElementById("btn");
-const emojiNameEl = document.getElementById("emoji-name");
+const imageContainerEl = document.querySelector(".image-container");
 
-const emoji = [];
-
-async function getEmoji() {
-  let response = await fetch(
-    "https://emoji-api.com/emojis?access_key=773b58f681fb786fafdb8392e8b8a75ddc177fd1"
-  );
-
-  data = await response.json();
-
-  for (let i = 0; i < 1500; i++) {
-    emoji.push({
-      emojiName: data[i].character,
-      emojiCode: data[i].unicodeName,
-    });
-  }
-}
-
-getEmoji();
+const btnEl = document.querySelector(".btn");
 
 btnEl.addEventListener("click", () => {
-  const randomNum = Math.floor(Math.random() * emoji.length);
-  btnEl.innerText = emoji[randomNum].emojiName;
-  emojiNameEl.innerText = emoji[randomNum].emojiCode;
+  imageNum = 10;
+  addNewImages();
 });
+
+function addNewImages() {
+  for (let index = 0; index < imageNum; index++) {
+    const newImgEl = document.createElement("img");
+    newImgEl.src = `https://picsum.photos/300?random=${Math.floor(
+      Math.random() * 2000
+    )}`;
+    imageContainerEl.appendChild(newImgEl);
+  }
+}
