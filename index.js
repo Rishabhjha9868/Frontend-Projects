fa7d6a48640af1e733921a9861bced7b8154266e
Rishabhjha9868 <147179883+Rishabhjha9868@users.noreply.github.com>
2023-11-03 19:58:31 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-const imageContainerEl = document.querySelector(".image-container");
+const btnEl = document.getElementById("btn");
+const quoteEl = document.getElementById("quote");
+const authorEl = document.getElementById("author");
 
-const btnEl = document.querySelector(".btn");
+const apiURL = "https://api.quotable.io/random";
 
-btnEl.addEventListener("click", () => {
-  imageNum = 10;
-  addNewImages();
-});
-
-function addNewImages() {
-  for (let index = 0; index < imageNum; index++) {
-    const newImgEl = document.createElement("img");
-    newImgEl.src = `https://picsum.photos/300?random=${Math.floor(
-      Math.random() * 2000
-    )}`;
-    imageContainerEl.appendChild(newImgEl);
+async function getQuote() {
+  try {
+    btnEl.innerText = "Loading...";
+    btnEl.disabled = true;
+    quoteEl.innerText = "Updating...";
+    authorEl.innerText = "Updating...";
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    const quoteContent = data.content;
+    const quoteAuthor = data.author;
+    quoteEl.innerText = quoteContent;
+    authorEl.innerText = "~ " + quoteAuthor;
+    btnEl.innerText = "Get a quote";
+    btnEl.disabled = false;
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+    quoteEl.innerText = "An error happened, try again later";
+    authorEl.innerText = "An error happened";
+    btnEl.innerText = "Get a quote";
+    btnEl.disabled = false;
   }
 }
+
+getQuote();
+
+btnEl.addEventListener("click", getQuote);
