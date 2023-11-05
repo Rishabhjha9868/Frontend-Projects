be30f94fa7853c4dbfcf26b3b3110d90f1b66c3f
Rishabhjha9868 <147179883+Rishabhjha9868@users.noreply.github.com>
2023-11-05 20:48:15 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const textareaEl = document.getElementById("textarea");
-const totalCounterEl = document.getElementById("total-counter");
-const remainingCounterEl = document.getElementById("remaining-counter");
+const navbarEl = document.querySelector(".navbar");
 
-textareaEl.addEventListener("keyup", () => {
-  updateCounter();
+const bottomContainerEl = document.querySelector(".bottom-container");
+
+console.log(navbarEl.offsetHeight);
+
+console.log(bottomContainerEl.offsetTop);
+
+window.addEventListener("scroll", () => {
+  if (
+    window.scrollY >
+    bottomContainerEl.offsetTop - navbarEl.offsetHeight - 50
+  ) {
+    navbarEl.classList.add("active");
+  } else {
+    navbarEl.classList.remove("active");
+  }
 });
-
-updateCounter();
-
-function updateCounter() {
-  totalCounterEl.innerText = textareaEl.value.length;
-  remainingCounterEl.innerText =
-    textareaEl.getAttribute("maxLength") - textareaEl.value.length;
-}
